f9bc79536a8dccd57bda08755dfaa009b84d1214
Francisco Silveira <FranciscoSilveira@users.noreply.github.com>
2016-11-18 22:28:10 +0000

    Update run2016.sh
    -Fixed the classpath YET AGAIN
    -Tests dir now in the same dir as the packages
    -Compilation is now a function, uses an array (because there's no global makefile)
    -Testing is now its own function, added comments for readability

--- run2016.sh
+++ run2016.sh
@@ -1,52 +1,77 @@
 #!/bin/bash
 
-classpath="-cp $(JAVADIR)/po-uilib.jar:../project/pex-core/pex-core.jar:../project/pex-app/pex-app.jar"
-mainclass="edt.textui.TextEditor"
-common=".*/A-([0-9]{3})-([0-9]{3})-M-ok."
+# Global variables, classpath may need to be user-defined
+classpath="-cp /usr/share/java/po-uuilib.jar:../pex-core/pex-core.jar:../pex-app/pex-app.jar"
+mainclass="pex.app.App"
+common=".*A-([0-9]{3})-([0-9]{3})-M-ok\."
 input="in"
 import="import"
 output="outhyp"
 expected="out"
 logfile="results.txt"
-tempfile="temp"
 
-make -C ../project/
-echo -n "" > $logfile
-failed=0
-passed=0
-echo -e "\nRunning tests\n"
-for inputfile in `find . -regextype posix-extended  -regex "$common""$input" | sort`
-do
-	commonfile=$(echo $inputfile | cut -d '.' -f 2 -s)
-	outputfile=".$commonfile.$output"
-	echo "" > $outputfile
-	javaoptions="-Din=$inputfile -Dout=$outputfile"
-	
-	importfile=".$commonfile.$import"
-	if [ -f $importfile ]
-	then
-		javaoptions="-Dimport=$importfile $javaoptions"
-	fi
+function compile {
+	make_dirs=("../pex-core/" "../pex-core/")
+	for dir in "${make_dirs[@]}"
+	do
+		make -C "$dir"
+		if [[ $? -ne 0 ]]
+		then
+			echo -e "\nCompilation failed on package ""$dir""\n"
+			exit 1
+		fi
+	done
+}
+
+function test {
+	echo -n "" > $logfile
+	failed=0
+	passed=0
+	echo -e "\nRunning tests...\n"
+	for inputfile in `find . -regextype posix-extended  -regex "$common""$input" | sort`
+	do
+		# Find the common substring for this test
+		commonfile=$(echo $inputfile | cut -d '.' -f 2 -s | cut -d '/' -f '2' -s)
 		
-	java $javaoptions $classpath $mainclass
+		# Create (or clear) the output file for this test
+		outputfile="$commonfile.$output"
+		echo "" > $outputfile
+		javaoptions="$classpath -Din=$inputfile -Dout=$outputfile"
+		
+		# Find and add an import file for this test, if it exists
+		importfile="$commonfile.$import"
+		echo "Import file: ""$importfile"
+		if [ -f $importfile ]
+		then
+			javaoptions="-Dimport=$importfile $javaoptions"
+		fi
+		
+		# Run the test
+		java $javaoptions $classpath $mainclass
+		
+		# Compare the obtained output to the expected
+		expectedfile="./expected/$commonfile.$expected"
+		echo -e "TEST: $commonfile\n\tOPTIONS:$javaoptions" >> $logfile
+		differences=`diff $outputfile $expectedfile`
+		if [[ -z $differences ]]
+		then
+			echo -e "\tPassed!\n" >> $logfile
+			passed=$((passed+1))
+		else
+			echo -e "\tFailed!" >> $logfile
+			differences=`echo -e "$differences" | sed 's/^/\t\t/'`
+			echo -e "$differences\n" >> $logfile
+			failed=$((failed+1))
+		fi
+	done
 	
-	expectedfile="./expected/$commonfile.$expected"
-	echo -e "TEST: $commonfile\n\tOPTIONS:$javaoptions" >> $logfile
-	differences=`diff $outputfile $expectedfile`
-	if [[ -z $differences ]]
-	then
-		echo -e "\tPassed!\n" >> $logfile
-		passed=$((passed+1))
-	else
-		echo -e "\tFailed!" >> $logfile
-		differences=`echo -e "$differences" | sed 's/^/\t\t/'`
-		echo -e "$differences\n" >> $logfile
-		failed=$((failed+1))
-	fi
-	
-done
-result="Passed:$passed\nFailed:$failed" 
-sed "1s/^/$result\n\n/" -i $logfile
-echo -e $result"\nSee file '$logfile' for more information"
+	# Print the results on stdout and append them to the logfile
+	result="Passed:$passed\nFailed:$failed" 
+	sed "1s/^/$result\n\n/" -i $logfile
+	echo -e $result"\nSee file '$logfile' for more information"
+}
+
+compile
+test
 
 exit 0
